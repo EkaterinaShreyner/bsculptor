08acdd77a28213f5333facd1a3ea00c18ef403b0
DmitryKro <dmitry.kropachev@functor.team>
2024-01-29 19:49:36 +0300

    Added mailer for send emails

---
+++ backend/mailer.js
@@ -0,0 +1,21 @@
+// require('dotenv').config();
+// const nodemailer = require('nodemailer')
+// const EMAIL = 'test@gmail.com'
+// const PASSWORD = 'afdsfdsf'
+
+// const transporter = nodemailer.createTransport({
+//     service: 'gmail',
+//     auth: {
+//         user: EMAIL,
+//         pass: PASSWORD
+//     }
+// })
+
+// const mailOptions = {
+//     from: EMAIL,
+//     to: EMAIL,
+//     subject: 'Письмо от bsculptor',
+//     text: 'Спасибо за выбор промо'
+// }
+
+// transporter.sendMail(mailOptions)
